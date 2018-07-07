b3fe19ee7d92de0d767eac0ea896a43b2face9ea
Huijie Wei <huijiewei@outlook.com>
2018-07-07 22:34:39 +0800

    增强 StringHelper 工具类 增加 PhoneNumberValidator 验证类

---
+++ src/core/validators/PhoneNumberValidator.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Huijiewei
+ * Date: 2017/1/5
+ * Time: 上午11:23
+ */
+
+namespace app\core\validators;
+
+use app\core\helpers\StringHelper;
+use yii\base\Model;
+use yii\validators\Validator;
+
+class PhoneNumberValidator extends Validator
+{
+    public $phoneNumber;
+
+    public function init()
+    {
+        parent::init();
+
+        if ($this->message == null) {
+            $this->message = '{attribute}格式无效';
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validateAttribute($model, $attribute)
+    {
+        parent::validateAttribute($model, $attribute);
+
+        /** @var Model $model */
+        if (!$model->hasErrors($attribute)) {
+            $model->$attribute = $this->phoneNumber;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function validateValue($value)
+    {
+        if (is_array($value)) {
+            $value = implode('', $value);
+        }
+
+        if (!StringHelper::isPhoneNumber($value)) {
+            return [$this->message, []];
+        }
+
+        $this->phoneNumber = $value;
+
+        return null;
+    }
+}
