9cfd02a7e95b6112a64e3c59d0c28fb81aca1e93
Huijie Wei <huijiewei@outlook.com>
2018-07-26 21:47:02 +0800

    搜索框组件基本完成

--- resources/modules/admin/components/SearchForm.vue
+++ resources/modules/admin/components/SearchForm.vue
@@ -3,22 +3,40 @@
            :model="formModel"
            size="small"
            autocomplete="off"
-           @submit.native.stop.prevent="handleFormSubmit()">
-    <el-form-item v-if="searchFields && searchFields.length > 0">
-      <el-input placeholder="请输入内容" v-model="formModel.keyword">
-        <el-select v-model="formModel.field" slot="prepend" :style="{width: '90px'}">
-          <template v-for="(item, index) in searchFields">
-            <el-option :key="index" :label="item.label" :value="item.field"></el-option>
+           @submit.native.stop.prevent="handleFormSubmit()"
+           @reset.native.stop.prevent="handleFormReset()">
+    <template v-if="getOtherFields.length> 0" v-for="(item, index) in getOtherFields">
+      <el-form-item :key="index">
+        <el-select
+          v-if="item.type==='select'"
+          v-model="formModel[item.field]"
+          :multiple="item.multiple"
+          :placeholder="item.label">
+          <template v-for="(optionLabel, optionValue, optionIndex) in item.options">
+            <el-option :key="index + '-' + optionIndex" :label="optionLabel" :value="optionValue"></el-option>
           </template>
         </el-select>
-      </el-input>
-    </el-form-item>
+      </el-form-item>
+    </template>
+    <template v-if="getKeywordFields.length > 0">
+      <el-form-item>
+        <el-input placeholder="请输入内容" v-model="formModel.keyword">
+          <el-select v-model="formModel.field" slot="prepend" :style="{width: '90px'}">
+            <template v-for="(item, index) in getKeywordFields">
+              <el-option :key="index" :label="item.label" :value="item.field"></el-option>
+            </template>
+          </el-select>
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button :plain="true" type="default" native-type="submit">
+          搜索
+        </el-button>
+      </el-form-item>
+    </template>
     <el-form-item>
-      <el-button :plain="true" type="info" native-type="submit">
-        搜索
-      </el-button>
-      <el-button :plain="true" type="default" native-type="reset">
-        <bp-icon type="database"></bp-icon>
+      <el-button :plain="true" type="info" native-type="reset">
+        重置
       </el-button>
     </el-form-item>
   </el-form>
@@ -30,35 +48,102 @@
   export default {
     name: 'SearchForm',
     components: { BpIcon },
-    props: ['searchFields'],
+    props: {
+      searchFields: Array | null
+    },
     data() {
       return {
-        formModel: {
-          field: this.$route.query.field || null,
-          keyword: this.$route.query.keyword || ''
+        formInit: false,
+        formModel: {}
+      }
+    },
+    computed: {
+      getKeywordFields() {
+        if (this.searchFields === null) {
+          return []
         }
+
+        return this.searchFields.filter(item => item.type === 'keyword')
+      },
+      getOtherFields() {
+        if (this.searchFields === null) {
+          return []
+        }
+
+        return this.searchFields.filter(item => (item.type !== 'keyword' && item.type !== 'hidden'))
       }
     },
     watch: {
-      'searchFields': 'updateDefaultField'
+      'searchFields': 'updateFormModel',
+      '$route': 'updateFormModel'
     },
     mounted() {
-      this.updateDefaultField()
+      if (!this.formInit) {
+        this.updateFormModel()
+      }
     },
     methods: {
-      updateDefaultField() {
-        if (this.formModel.field === null && this.searchFields && this.searchFields.length > 0) {
-          this.formModel.field = this.searchFields[0].field
+      updateFormModel() {
+        const routeQuery = this.$route.query
+        const formModel = {}
+
+        if (this.getKeywordFields.length > 0) {
+          formModel.field = routeQuery.field || this.getKeywordFields[0].field
+          formModel.keyword = routeQuery.keyword || ''
         }
+
+        if (this.getOtherFields.length > 0) {
+          this.getOtherFields.forEach((item) => {
+            formModel[item.field] = routeQuery[item.field] || (item.multiple ? [] : '')
+          })
+        }
+
+        this.formInit = true
+        this.formModel = formModel
+      },
+      getQueryFields() {
+        const queryFields = {}
+
+        Object.keys(this.formModel).forEach((key) => {
+          if (key !== 'field' && this.formModel[key] && this.formModel[key].length > 0)
+            queryFields[key] = this.formModel[key]
+        })
+
+        if (Object.keys(queryFields).length > 0 && this.formModel.hasOwnProperty('field')) {
+          queryFields['field'] = this.formModel['field']
+        }
+
+        Object.keys(this.$route.query).forEach((key) => {
+          if (!this.formModel.hasOwnProperty(key) && key !== 'page') {
+            queryFields[key] = this.$route.query[key]
+          }
+        })
+
+        return queryFields
+      },
+      getDefaultQuery() {
+        const defaultQuery = {}
+
+        Object.keys(this.$route.query).forEach((key) => {
+          if (!this.formModel.hasOwnProperty(key) && key !== 'page') {
+            defaultQuery[key] = this.$route.query[key]
+          }
+        })
+
+        return defaultQuery
       },
       handleFormSubmit() {
+        const queryFields = this.getQueryFields()
+
         this.$router.push({
-          path: this.$route.fullPath,
-          query: {
-            field: this.formModel.field,
-            keyword: this.formModel.keyword,
-            page: null
-          }
+          path: this.$route.path,
+          query: queryFields
+        })
+      },
+      handleFormReset() {
+        this.$router.push({
+          path: this.$route.path,
+          query: this.getDefaultQuery()
         })
       }
     }
--- src/modules/admin/api/models/UserSearchFrom.php
+++ src/modules/admin/api/models/UserSearchFrom.php
@@ -13,6 +13,15 @@ use app\core\models\user\User;
 
 class UserSearchFrom extends SearchForm
 {
+    public $createdFrom;
+
+    public function rules()
+    {
+        return array_merge([
+            ['createdFrom', 'trim']
+        ], parent::rules());
+    }
+
     protected function getQuery()
     {
         $userQuery = User::find()->orderBy(['id' => SORT_DESC]);
@@ -27,6 +36,10 @@ class UserSearchFrom extends SearchForm
             }
         }
 
+        if (!empty($this->createdFrom)) {
+            $userQuery->andWhere(['createdFrom' => $this->createdFrom]);
+        }
+
         return $userQuery;
     }
 
@@ -34,13 +47,22 @@ class UserSearchFrom extends SearchForm
     {
         return [
             [
+                'type' => 'keyword',
                 'field' => 'phone',
                 'label' => '电话号码',
             ],
             [
+                'type' => 'keyword',
                 'field' => 'display',
                 'label' => '显示名',
             ],
+            [
+                'type' => 'select',
+                'field' => 'createdFrom',
+                'label' => '注册来源',
+                'multiple' => true,
+                'options' => User::createdFromNameList(),
+            ]
         ];
     }
 }
