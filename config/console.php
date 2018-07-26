ffea3f552bc796718188474507244737bedb3b83
Huijie Wei <huijiewei@outlook.com>
2018-07-26 09:08:42 +0800

    增加 app\core\commands 控制器命名空间

--- config/console.php
+++ config/console.php
@@ -11,6 +11,8 @@ $common = require(__DIR__ . '/common.php');
 $config = [
     'id' => getenv('APP_ID') . '-console',
 
+    'controllerNamespace' => 'app\\core\\commands',
+
     'components' => [
         'urlManager' => [
             'baseUrl' => getenv('APP_URL'),
