888df81a7eef53489181906dae98e2777dc75b20
Huijie Wei <huijiewei@outlook.com>
2020-09-21 15:53:22 +0800

    更新主页

--- modules/admin/views/site/Index.vue
+++ modules/admin/views/site/Index.vue
@@ -1,23 +1,5 @@
 <template>
   <div class="box">
-    <h4>特点</h4>
-    <ol>
-      <li>
-        <p>基于 VUE CLI</p>
-      </li>
-      <li>
-        <p>自带 Mock 服务器</p>
-      </li>
-      <li>
-        <p>使用 .env 配置环境变量</p>
-      </li>
-    </ol>
-    <p>
-      <a href="https://github.com/huijiewei/agile-vue" target="_blank"
-        >访问 Github 项目地址</a
-      >
-    </p>
-    <hr />
     <el-row :gutter="15">
       <el-col :span="6">
         <el-card class="box-card">
